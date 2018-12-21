e9f71d703186f113c96c0eca08ec1a3445defc2c
x-joyce <15735045716@163.com>
2018-12-21 11:43:51 +0800

    xx

--- test/src/router/index.js
+++ test/src/router/index.js
@@ -1,65 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ButtonCounter from '@/components/ButtonCounter'
-import Index from '@/components/index'
-import BlogPost from '@/components/BlogPost'
-import CustomInput from '@/components/CustomInput'
-import Table from '@/components/table'
-import Nav from '@/components/nav'
-import Upload from '@/components/upload'
-import DateTime from '@/components/DateTime'
-import Slot from '@/components/slot'
 import Prop from '@/components/prop'
 import Test from '@/components/test'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path:"/",
-      name:'ButtonCounter',
-      component:ButtonCounter
-    },
-    {
-      path:"/index",
-      name:'index',
-      component:Index
-    },
-    {
-      path:"/BlogPost",
-      name:"BlogPost",
-      component:BlogPost
-    },
-    {
-      path:"/custom",
-      name:"CustomInput",
-      component:CustomInput
-    },
-    {
-      path:"/table",
-      name:"table",
-      component:Table
-    },
-    {
-      path:"/nav",
-      name:"nav",
-      component:Nav
-    },
-    {
-      path:"/upload",
-      name:"upload",
-      component:Upload
-    },
-    {
-      path:"/datetime",
-      name:"DateTime",
-      component:DateTime
-    },
-    {
-      path:"/slot",
-      name:"slot",
-      component:Slot
-    },
     {
       path:"/prop",
       name:"prop",
